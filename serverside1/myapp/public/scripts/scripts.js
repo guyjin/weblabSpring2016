03193397378283d3fcff57ffc566c6ac74da6f05
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-03-02 23:33:56 -0700

    added js script
    added small script for client side form validation

--- serverside1/myapp/public/scripts/scripts.js
+++ serverside1/myapp/public/scripts/scripts.js
@@ -1,9 +1,15 @@
 $(function() {
+        // script runs on every page, but is looking for the input field of the About page.
         if($('#myReasonForm')) {
+                // when the form is found, grab the form
             var myForm = $('#myReasonForm');
-            console.log(myForm);
+
+            // listen for a submit event on the form.
             myForm.on('submit', function(e) {
-                    console.log($('#myReason').val());
+
+                //     console.log($('#myReason').val());
+                // if there's no value in the input box,
+                // stop the submission and throw up an alert.
                 if($('#myReason').val() === '') {
                     e.preventDefault();
                     alert('You must enter a good reason.  Why are you awesome?');
